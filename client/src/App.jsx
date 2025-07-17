392eeeb6f24d8a33131fe7be7fde5d569975b9f2
Muhammad Ibraheem <muhammadibraheem8567@gmail.com>
2025-07-17 17:37:25 +0500

    routing-done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,53 @@
-import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './components/Layout/Layout'
+import { Error } from './pages/Error/Error'
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
+import Contact from './pages/Contact/Contact'
+import Login from './pages/Login/Login'
+import Register from './pages/Register/Register'
+import Logout from './pages/Logout/Logout'
 
-function App() {
+
+export default function App() {
+
+  const router = createBrowserRouter(
+    [
+      {
+        path: '/',
+        element: <Layout />,
+        errorElement: <Error />,
+        children:[
+          {
+            path: '/',
+            element: <Home />
+          },
+          {
+            path: '/about',
+            element: <About />
+          },
+          {
+            path: '/contact',
+            element: <Contact />
+          },
+          {
+            path: '/login',
+            element: <Login />
+          },
+          {
+            path: '/register',
+            element: <Register />
+          },
+          {
+            path: '/logout',
+            element: <Logout />
+          },
+        ]
+      }
+    ]
+  );
 
   return (
-    <>
-        <h1>
-          Donor Recommedation System
-        </h1>
-    </>
+    <RouterProvider router={router}/>
   )
 }
-
-export default App
---
+++ client/src/pages/Logout/Logout.jsx
@@ -0,0 +1,9 @@
+function Logout(){
+    return(
+        <>
+            <h1>I am Logout</h1>
+        </>
+    )
+}
+
+export default Logout;
